8812ae1d1e57b96f3aba8ba0dc876f282d38c83f
pawaffle <pdegterev@gmail.com>
2024-09-15 16:20:48 +0300

    simplified footer

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,45 +1,34 @@
+import PageLinks from "./PageLinks";
 import React from "react";
 
-const NavigationItem = ({ text }) => <div className="mt-3.5">{text}</div>;
-
 const Footer = () => {
-  const navigationItems = ["About", "Advice", "Jobs"];
+  
+    return (
+        <footer className="flex flex-col items-center p-11 w-full bg-indigo-950 max-md:px-5 max-md:max-w-full">
+            <div className="flex justify-between items-center p-4 w-full max-w-6xl text-white">
 
-  return (
-    <footer className="flex overflow-hidden flex-col bg-white">
-      <div className="flex flex-col items-center px-20 pt-20 pb-11 w-full bg-indigo-950 max-md:px-5 max-md:max-w-full">
-        <div className="flex flex-col ml-9 max-w-full w-[812px]">
-          <div className="max-md:max-w-full">
-            <div className="flex gap-5 max-md:flex-col">
-              <div className="flex flex-col w-[42%] max-md:ml-0 max-md:w-full">
-                <h1 className="pt-0.5 text-6xl text-center text-white max-md:pr-2 max-md:mt-10 max-md:text-4xl">
-                  JobScout
-                </h1>
-              </div>
-              <nav className="flex flex-col ml-5 w-1/5 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col grow items-start mt-7 text-2xl text-white whitespace-nowrap max-md:mt-10">
-                  <h2 className="self-stretch font-semibold">Navigation</h2>
-                  {navigationItems.map((item, index) => (
-                    <NavigationItem key={index} text={item} />
-                  ))}
+                <div class="relative w-[40%] h-32 pt-0.5 text-6xl text-white max-md:pr-2 max-md:mt-10 max-md:text-4xl">
+                    <div class="absolute top-0 right-0 text-right p-2">
+                        Job$cout</div>
                 </div>
-              </nav>
-              <address className="flex flex-col ml-5 w-[38%] max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col items-start self-stretch my-auto text-2xl text-white max-md:mt-10">
-                  <h2 className="font-semibold">Contact us</h2>
-                  <div className="self-stretch mt-3.5">main@jobscout.com</div>
-                  <div className="mt-4">+35837502445</div>
+                
+                <div className="w-[20%] text-center">
+                    <PageLinks parentClass="flex flex-col grow mt-7 text-2xl text-white whitespace-nowrap max-md:mt-10" itemClass="hover:text-blue-700" />
                 </div>
-              </address>
+                
+                <div className="w-[40%] text-start pl-2">
+                    <p className="font-semibold">Contact us:</p>
+                    <p className="self-stretch mt-2">main@jobscout.com</p>
+                    <p>+358-45-101-00-11</p>
+                </div>
+
             </div>
-          </div>
-          <div className="self-center mt-32 text-2xl font-semibold text-white max-md:mt-10">
-            Made by team 1
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-};
 
-export default Footer;
+            <div className="self-center mt-10 text-2xl font-semibold text-white max-md:mt-10">
+                Made by team 1
+            </div>
+        </footer>
+    );
+  };
+  
+  export default Footer;
