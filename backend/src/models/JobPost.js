299412ccfd6ad813dd1a11ae769a9876c554ff7a
viettranni <vttranviett@gmail.com>
2024-09-19 00:12:18 +0300

    Pulling the new job model from the main

--- backend/src/models/JobPost.js
+++ backend/src/models/JobPost.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 // Define the schema for the job post
-const jobPostSchema = new mongoose.Schema({
-    title: String,
-    company: String,
-    location: String,
-    datePosted: String,
-    url: String,
-    description: String,
-    responsibilities: String,
-    logo: String
-
-}, { timestamps: true });
+const jobPostSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    location: { type: String, required: true },
+    datePosted: { type: String, required: true },
+    url: { type: String, required: true, unique: true },
+    description: { type: [String] },
+    responsibilities: { type: [String] },
+    logo: { type: String },
+  },
+  {
+    timestamps: true, // Adds `createdAt` and `updatedAt` fields
+  }
+);
 
 // Create a model based on the schema
 const JobPost = mongoose.model("JobPost", jobPostSchema);
 
-module.exports = JobPost;
+module.exports = { JobPost, jobPostSchema };
