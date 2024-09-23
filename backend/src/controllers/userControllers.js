447fd6d22e78dcc91220dd5d3d7b8bd3b741645d
viettranni <vttranviett@gmail.com>
2024-09-23 22:56:54 +0300

    Learned from the lectures that the register and login can be implemented via Model statics also so I made the necessary modifications and also deleted exccessive code and file

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -172,3 +172,29 @@ exports.getUserWithFavourites = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+
+exports.registerUser = async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+  
+  try {
+    const newUser = await User.signup(firstname, lastname, email, password, [], []);
+    res.status(201).json({ message: "User created successfully!" });
+    console.log('New user registered:', newUser);
+  } catch (error) {
+    console.error('Error in registerUser:', error.message);  // Log error message
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const loggedInUser = await User.login(email, password);
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (error) {
+    console.error('Error in loginUser:', error.message);  // Log error message
+    res.status(400).json({ error: error.message });
+  }
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -17,19 +17,44 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
+// userSchema.pre("save", async function (next) {
+//   if (this.isModified("password")) {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//   }
+//   next();
+// });
 
-// Compare password method
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
+// // Compare password method
+// userSchema.methods.comparePassword = function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
+
+// const User = mongoose.model("User", userSchema);
+
+// Static method for signup
+userSchema.statics.signup = async function (firstname, lastname, email, password, favourites, appliedJobs) {
+  const userExists = await this.findOne({ email });
+  if (userExists) throw new Error('User already exists');
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ firstname, lastname, email, password: hashedPassword, favourites, appliedJobs });
+  return user;
 };
 
-const User = mongoose.model("User", userSchema);
+// Static method for login
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (!user) throw new Error('Invalid email or password');
 
-module.exports = User;
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error('Invalid email or password');
+
+  return user;
+};
+
+
+// module.exports = User;
+module.exports = mongoose.model('User', userSchema);
