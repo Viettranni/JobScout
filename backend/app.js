7ca2c9acb321ab37d1fc82a203ad38ff93fbfe75
Saed Abukar <saed.a.abukar@gmail.com>
2024-10-09 12:14:51 +0300

    added helmet and rate limiter

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const connectDB = require("../backend/src/config/db");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const authRoutes = require("./src/routes/authRouter");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const adminRoutes = require("./src/routes/adminRouter");
 const jobRoutes = require("./src/routes/jobRouter");
 const userRoutes = require("./src/routes/userRouter");
@@ -29,6 +30,16 @@ app.use(
     origin: process.env.CORS_ORIGIN || "http://localhost:5173", // Allowing the Frontend to interact with backend
   })
 );
+
+app.use(helmet()); // Adding Helmet for security
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+
+app.use(limiter); // Applying rate limiting to all requests
 
 app.use(express.json());
 
