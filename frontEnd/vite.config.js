ad8f65ac87e1b7218c7daaa3e8c1730602fca843
viettranni <vttranviett@gmail.com>
2024-10-12 01:12:27 +0300

    Configuring the port to bind the server to 0.0.0.0

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: '0.0.0.0', // Bind to all IP addresses
+    port: process.env.PORT || 5173, // Use Render's assigned port if available, fallback to 5173
+  },
+});
